ae7591d35ccb486baae6bebf299066ad2103f340
SenilDevx <senil.mendapara@devxconsultancy.com>
2026-02-22 17:00:30 +0530

    add digit only check

--- src/modules/auth/dto/verify-otp.dto.ts
+++ src/modules/auth/dto/verify-otp.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, Length } from 'class-validator';
+import { IsString, Matches } from 'class-validator';
 
 export class VerifyOtpDto {
   @ApiProperty({ example: '123456' })
   @IsString()
-  @Length(6, 6, { message: 'token must be exactly 6 digits' })
+  @Matches(/^\d{6}$/, { message: 'token must be exactly 6 digits' })
   token!: string;
 }
